2b188374f9becc74eb91a72500c56263226fcdbe
MaciekTalaska <maciek.talaska@gmail.com>
2019-07-22 15:17:26 +0200

    move PESEL and PESELParsingError into separate modules

---
+++ src/pesel_parsing_error.rs
@@ -0,0 +1,25 @@
+use std::error::Error;
+
+#[derive(Debug)]
+pub struct PESELParsingError {
+    message: String
+}
+
+impl PESELParsingError {
+    pub fn new (msg: &str) -> PESELParsingError {
+        PESELParsingError{ message: msg.to_string() }
+    }
+}
+
+impl std::fmt::Display for PESELParsingError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f,"{}",self.message)
+    }
+}
+
+impl Error for PESELParsingError {
+    fn description(&self) -> &str {
+        &self.message
+    }
+}
+
